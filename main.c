382d254cb925d376b57f3e94581f27e370ce054d
Garrett Greenwood <garrettagreenwood@gmail.com>
2016-10-02 15:03:57 -0500

    Added left/right states
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- main.c
+++ main.c
@@ -8,6 +8,19 @@
 #define CLEAR_BIT(TGT, PIN)  do { TGT &= ~(1 << (PIN)); } while(0)
 #define TOGGLE_BIT(TGT, PIN) do { TGT ^=  (1 << (PIN)); } while(0)
 
+#define LEFT_WAIT   4
+#define RIGHT_WAIT  4
+
+typedef enum {
+    Idle_left,
+    Idle_right,
+    Move_left,
+    Move_right
+} State;
+
+int state = 0;
+int count = 0;
+
 int main(void)
 {
     wdt_enable(WDTO_1S);    // enable 1s watchdog timer
@@ -32,25 +45,27 @@ int main(void)
     while(1) {
         wdt_reset();
 
-        DDRC = 0b00000001;
-        PORTC = 0b000000010;
-        _delay_us(16500);
-
-        DDRC = 0b00000010;
-        PORTC = 0b000000001;
-        _delay_us(16500);
+        if(state == Move_left) {
+            DDRC = 0b00000001;
+            PORTC = 0b11111110;
+            state = 1;
+        } else if(state == Move_right) {
+            DDRC = 0b00000010;
+            PORTC = 0b11111101;
+            state = 0;
+        }
     }
 }
 
 // Watch the USB D- line for activity, so we can stay synchronized
 ISR(INT0_vect) {
     // Wait out a sync pulse's duration
-    _delay_us(1);
+    _delay_us(15);
 
     // Clear the interrupt flag, so we won't double-trigger on previous pulses
     SET_BIT(EIFR, INTF0);
 
-    // Wait out another 2us
+    // Wait out another 2us, to give INTF0 a chance to trigger
     _delay_us(2);
 
     // See if there's more activity on D-
@@ -58,7 +73,15 @@ ISR(INT0_vect) {
     if(GET_BIT(EIFR, INTF0)) {
         // Toggle the debug line
         SET_BIT(PORTD, PD3);
-        _delay_us(5);
+        _delay_us(20);
         CLEAR_BIT(PORTD, PD3);
+        count++;
+        if(count >= LEFT_WAIT & state == Idle_left) {
+            state = Move_left;
+            count = 0;
+        } else if(count >= RIGHT_WAIT & state == Idle_right) {
+            state = Move_right;
+            count = 0;
+        }
     }
 }
