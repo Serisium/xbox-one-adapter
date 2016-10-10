2195e7babc5a6ea7e209cc2470d5888b9fb22a8b
Garrett Greenwood <garrettagreenwood@gmail.com>
2016-10-10 03:24:52 -0500

    Finished initial controller polling in C
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- controller.c
+++ controller.c
@@ -52,8 +52,9 @@
         )
 
 uint8_t gc_poll(uint8_t *controller_buffer) {
-    controller_buffer[0] = 0b10101010;
-    controller_buffer[1] = 0b01010101;
+    for(uint8_t i = 0; i < 8; i++) {
+        controller_buffer[i] = 0b10101010;
+    }
 
     SET_BIT(PORT_GC, P_GC);
     SET_BIT(DDR_GC, P_GC);          // Set PIN_GC as output
@@ -79,48 +80,62 @@ uint8_t gc_poll(uint8_t *controller_buffer) {
 
     // '0' bits usually take 9-10 loops
     // '1' bits usually take 3-4 loops
+    // i holds the timekeeping/timeout value
+    // n holds the current bit being written(0b00000001 -> 0b00000010, etc)
+    // __tmp_reg__ holds the current byte, before being written to the buffer
     asm volatile(
             //"ld %[i], %a[buff]+ \n\t"
-            "ldi __tmp_reg__, 0 \n\t"
-            "ldi %[i], 5 \n\t"
-            "rjmp low \n\t"
+            "mov __tmp_reg__, __zero_reg__ \n\t"
+            "ldi %[n], 0b10000000 \n\t"
+            "ldi %[i], 3 \n\t"
+            "rjmp signal \n\t"
             "loop: ldi %[i], 0xFF \n\t"         // Initialize timeout register
             "high: \n\t"   // Wait for the signal to go low
                 "dec %[i] \n\t"                 // Decrement the timeout register
                 "sbic %[pin_gc], %[bit_gc] \n\t"// Skip out if the signal goes low(bit is being transmitted)
                 "brne high \n\t"                // Loop back to 0
-            "breq error \n\t"                   // If we timed out instead of going low, quit
+            "breq end \n\t"                     // If we timed out instead of going low, quit
             "ldi %[i], %[threshold] \n\t"
-            "low: \n\t"   // Main bit-testing loop. Takes 4n-1 clock cycles
+            "signal: \n\t"   // Main bit-testing loop. Takes 4n-1 clock cycles
                 "dec %[i] \n\t"                 // 1c;      Decrement the timekeeping register
                 "sbis %[pin_gc], %[bit_gc] \n\t"// 1/2c;    Break out once the signal goes low
-                "brne low \n\t"                 // 2/1c;    Loop backwards unless we've timed out
+                "brne signal \n\t"              // 2/1c;    Loop backwards unless we've timed out
             "breq zero \n\t"
             "one:  \n\t"
+                "or __tmp_reg__, %[n] \n\t"
                 "sbi %[port_debug], %[bit_debug] \n\t"
                 "cbi %[port_debug], %[bit_debug] \n\t"
             "zero:  \n\t"
-            "0: \n\t"   // Wait for the signal to go low
-                "sbis %[pin_gc], %[bit_gc] \n\t"  // Skip out if the signal goes low(bit is being transmitted)
-                "brne 0b \n\t"            // Loop back to 0
+            "lsr %[n] \n\t" // Move on to the next bit in the series
+            "brne low \n\t"     // Check if we've filled a byte
+            "byte: \n\t"
+                "sbi %[port_debug], %[bit_debug] \n\t"
+                "st %a[buff]+, __tmp_reg__ \n\t"        // Store the bit in the buffer
+                "ldi %[n], 0b10000000 \n\t"             // Reinitialize the current bit
+                "mov __tmp_reg__, __zero_reg__ \n\t"
+                "cbi %[port_debug], %[bit_debug] \n\t"
+                //"rjmp end \n\t"
+            "low: \n\t"   // Wait for the signal to go low
+                "sbis %[pin_gc], %[bit_gc] \n\t"  // Skip out if the signal goes low(bit is still being transmitted)
+                "brne low \n\t"            // Loop back to 0
             "rjmp loop \n\t"
-            "error: ldi %[i], 0b10101010 \n\t"
+            "end: ldi %[i], 0b10101010 \n\t"
             :
             [i]                 "=a" (i),
-            [n]                 "=a" (n)
+            [n]                 "=a" (n),
+            [buff]              "+e" (controller_buffer)
             :
             [port_debug]        "I" (_SFR_IO_ADDR(PORT_DEBUG)),
             [bit_debug]         "I" (P_DEBUG),
             [port_gc]           "I" (_SFR_IO_ADDR(PORT_GC)),
             [pin_gc]            "I" (_SFR_IO_ADDR(PIN_GC)),
             [bit_gc]            "I" (P_GC),
-            [buff]              "e" (controller_buffer),
             [threshold]         "M" (7)
         );
 
     //CLEAR_BIT(PORT_DEBUG, P_DEBUG);
 
-    spi_transmit(i);
+    spi_transmit(controller_buffer[-8]);
 
     return 1;
 }
