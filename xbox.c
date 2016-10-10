ffe65635a8353e03174bc2b5e3d547825fe5bf68
Garrett Greenwood <garrettagreenwood@gmail.com>
2016-10-10 14:47:52 -0500

    fixed inverted Y axis
    Signed-off-by: Garrett Greenwood <garrettagreenwood@gmail.com>

--- xbox.c
+++ xbox.c
@@ -2,9 +2,9 @@
 
 void xbox_send(uint8_t *controller_buffer) {
     analog_write(ANALOG_0, controller_buffer[2]);
-    analog_write(ANALOG_1, controller_buffer[3]);
+    analog_write(ANALOG_1, -controller_buffer[3]);
     analog_write(ANALOG_2, controller_buffer[6]);
     analog_write(ANALOG_3, controller_buffer[4]);
-    analog_write(ANALOG_4, controller_buffer[5]);
+    analog_write(ANALOG_4, -controller_buffer[5]);
     analog_write(ANALOG_5, controller_buffer[7]);
 }
